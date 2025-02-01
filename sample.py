20870cb28ccb9cd182851e8cf22bd819e0447cde
marimo <kajisyo@marimox.net>
2025-02-02 00:18:31 +0900

    保存処理にコメントを追加

--- sample.py
+++ sample.py
@@ -7,5 +7,6 @@ wc = WindowCapture()
 selected_hwnd, selected_title = wc.select_window()
 print(f"選択されたウィンドウ: [{selected_hwnd}] {selected_title}")
 
+# 選択したウィンドウの画像を取得して保存
 image = wc.capture_window_area(selected_hwnd)
 image.save("output.png")
